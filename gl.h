9140e4d581487e0c88a321185ba1f816f0e9dff8
Abhishek <abhishek.dahad@gmail.com>
2025-05-27 22:53:13 -0400

    Split main.cpp into multiple files for better organization of code.

---
+++ gl.h
@@ -0,0 +1,28 @@
+#ifndef GL
+#define GL
+
+void line(vec2 p1, vec2 p2, TGAImage &image, TGAColor color);
+
+void hollow_triangle(vec2 p1, vec2 p2, vec2 p3, TGAImage &image, TGAColor color);
+
+void traditional_triangle(vec2 p1, vec2 p2, vec2 p3, TGAImage &image, TGAColor color);
+
+double calculate_triangle_area(vec2 p1, vec2 p2, vec2 p3);
+
+vec2 projection_on_screen(vec3 p, uint32_t offset);
+
+void triangle(vec3 p1, vec3 p2, vec3 p3, vec3 texture_index, std::vector<vec3> &texture_coodinates, uint32_t offset, TGAImage &texture_map,TGAImage &image, int width, int z_buffer[]);
+
+void parse_obj(std::string filename, std::vector<vec3> &vertices, std::vector<vec3> &texture_coodinates, std::vector<vec3> &texture_indices,std::vector<vec3> &faces);
+
+void scale_obj(uint32_t scale_factor, std::vector<vec3> &vertices);
+
+double calculate_angle(double coordinate_1, double coordinate_2);
+
+void rotate_obj(char axis, double angle, std::vector<vec3> &vertices);
+
+void wireframe_render(std::string filename, uint32_t scale_factor, uint32_t offset, TGAImage &image, TGAColor color);
+
+void solid_render(std::string filename, std::string texture_map_name,uint32_t scale_factor, uint32_t offset, TGAImage &image, TGAColor color, int width, int zbuffer[]);
+
+#endif
