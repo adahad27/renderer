d6c04b9f1d444d959cab999ccac8297ed43c2c6b
Abhishek <abhishek.dahad@gmail.com>
2025-08-05 14:44:31 -0400

    Started adding async IO to handle input from std::in

--- interface.cpp
+++ interface.cpp
@@ -4,6 +4,8 @@
 #define CMD_LOAD "load"
 #define CMD_ROTATE "rotate"
 #define CMD_SCALE "scale"
+#define PORT 8000
+#define MAX_MSG_LEN 1000
 
 //quit
 bool check_quit(std::vector<std::string> &args) {
@@ -49,38 +51,116 @@ bool check_scale(std::vector<std::string> &args) {
     }
 }
 
+void add_connection(int fd, std::vector<pollfd> &connections) {
 
-void start_IO_loop() {
-    
-    std::string input;
-    std::string arg;
+}
 
-    std::vector<std::string> args;
+void delete_connection(int fd, std::vector<pollfd> &connections) {
+
+}
+
+
+int init_server() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    fcntl(fd, F_SETFL, O_NONBLOCK);
+
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &val, sizeof(val));
+
+    sockaddr_in addr{};
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(PORT);
+
+
+    bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+    return fd;
+}
+
+void run_server(int fd) {
+    listen(fd, MAX_MSG_LEN);
+
+    struct sockaddr_in client{};
+    socklen_t addrlen = sizeof(client);
+
+
+
+    std::vector<pollfd> connections;
+
+    add_connection(fd, connections);
     
 
     while(true) {
-        std::getline(std::cin, input);
-        std::istringstream buffer(input);
 
-        while(std::getline(buffer, arg, ' ')) {
-            args.push_back(arg);
-        }
-        if(input.compare(CMD_QUIT)) {
-            std::cout << "Application closed!\n";
-            return;
-        }
-        else if(input.compare(CMD_LOAD)) {
 
-        }
-        else if(input.compare(CMD_ROTATE)) {
+        poll(connections.data(), connections.size(), -1);
 
-        }
-        else if(input.compare(CMD_SCALE)) {
 
+        for(uint32_t i = 0; i < connections.size(); ++i) {
+            if(connections[i].revents & POLLIN) {
+
+                if(connections[i].fd == fd) {
+                    int incoming_fd = accept(fd, (sockaddr*) &client, &addrlen);
+                    add_connection(incoming_fd, connections);
+                    connections[i].revents = 0;
+                    std::cout << "Connection established on socket: " << incoming_fd << std::endl;
+                }
+                else {
+                    //Insert read callback here
+                    handle_read(states[i]);
+                    delete_connection(i, connections);
+                    int socket_fd = connections[i].fd;
+                    close(socket_fd);
+                    std::cout << "Connection closed on socket: " << socket_fd << std::endl;
+                }
+            }
         }
-        else {
-            std::cout << "Unrecognized Command\n";
-        }
-        args.clear();
+
     }
+
+
+
+
+}
+
+void start_IO_loop() {
+    
+
+    int fd = init_server();
+    run_server(fd);
+
+
+
+    // std::string input;
+    // std::string arg;
+
+    // std::vector<std::string> args;
+    
+
+    // while(true) {
+    //     std::getline(std::cin, input);
+    //     std::istringstream buffer(input);
+
+    //     while(std::getline(buffer, arg, ' ')) {
+    //         args.push_back(arg);
+    //     }
+    //     if(input.compare(CMD_QUIT)) {
+    //         std::cout << "Application closed!\n";
+    //         return;
+    //     }
+    //     else if(input.compare(CMD_LOAD)) {
+
+    //     }
+    //     else if(input.compare(CMD_ROTATE)) {
+
+    //     }
+    //     else if(input.compare(CMD_SCALE)) {
+
+    //     }
+    //     else {
+    //         std::cout << "Unrecognized Command\n";
+    //     }
+    //     args.clear();
+    // }
 }
--- interface.h
+++ interface.h
@@ -1,13 +1,22 @@
 #ifndef INTERFACE
 #define INTERFACE
 
-#include <iostream>
-#include <string>
-#include <sstream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <cassert>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <poll.h>
 #include <vector>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <string>
 
-
-
+#include <iostream>
+#include <sstream>
 void start_IO_loop();
 
 
